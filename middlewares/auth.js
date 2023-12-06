5410f6f191e07b1da18be357eceb0ea9e9a5b489
Benjamin-89111 <ben999112@gmail.com>
2023-12-06 16:03:38 +0530

    backend_fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,32 +5,44 @@ import jwt from "jsonwebtoken";
 
 // Checks if user is authenticated or not
 export const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
+  const authorization = req.headers.authorization;
 
-  if (!token) {
-   return res.status(401).json({
+  if (!authorization || !authorization.startsWith('Bearer')) {
+    return res.status(401).json({
       success: false,
       message: "Login first to access this resource",
     });
-  
   }
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = await User.findById(decoded.id);
+  const token = authorization.split(' ')[1];
 
-  next();
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Login first to access this resource",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
 });
 
 // Authorize user roles
 export const authorizeRoles = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-     return res.status(403).json(
-        {
-          success: false,
-          message: `Role: ${req.user.role} is not allowed to access this resource`,
-        })
-      
+      return res.status(403).json({
+        success: false,
+        message: `Role: ${req.user.role} is not allowed to access this resource`,
+      });
     }
 
     next();
