ef65b3472371175511375e10cd376266e5f7890d
Benjamin-89111 <ben999112@gmail.com>
2023-12-07 11:45:01 +0530

    fixed multiple filter

--- utils/apiFilters.js
+++ utils/apiFilters.js
@@ -17,57 +17,44 @@ class APIFilters {
 
   filters() {
     const queryCopy = { ...this.queryStr };
-
+  
     // Fields to remove
     const fieldsToRemove = ["keyword", "page"];
     fieldsToRemove.forEach((el) => {
       delete queryCopy[el];
     });
-
+  
     Object.keys(queryCopy).forEach((key) => {
       const newKey = `attributes.${key}`;
+  
       if (key === "min" || key === "max") {
-
-        if(key==="min"){
-          queryCopy["attributes.price"] = { $gte: Number(queryCopy[key])};
-          delete queryCopy[key];
-        }
-        if(key==="max"){
+        if (key === "min") {
+          queryCopy["attributes.price"] = { $gte: Number(queryCopy[key]) };
+        } else if (key === "max") {
           queryCopy["attributes.price"] = { $lte: Number(queryCopy[key]) };
-          delete queryCopy[key];
         }
-
-        // if ((queryCopy[key])?.includes(',')){
-        //   queryCopy[newKey] = { $gte: Number(queryCopy[key].split(',')[0]), $lte: Number(queryCopy[key].split(',')[1]) };
-
-        // }
-        // else {
-        //   queryCopy[newKey] = Number(queryCopy[key]);
-        // }
-
-        // delete queryCopy[key];
-
-      } 
-      else if(key=="rating") {
+        delete queryCopy[key];
+      } else if (key === "rating") {
         queryCopy[newKey] = { $gte: Number(queryCopy[key]) };
         delete queryCopy[key];
-      }
-      
-      
-      
-      else {
+      } else if (key === "category") {
+        // Handle multiple values for category (comma-separated)
+        const values = queryCopy[key].split(",");
+        queryCopy["$or"] = values.map(value => ({ [newKey]: value }));
+        delete queryCopy[key];
+      } else {
         queryCopy[newKey] = queryCopy[key];
         delete queryCopy[key];
       }
     });
-
+  
     // Advanced filter for price, ratings, etc.
     let queryStr = JSON.stringify(queryCopy);
-
     this.query = this.query.find(JSON.parse(queryStr));
+  
     return this;
   }
-
+  
   pagination(resPerPage) {
     const currentPage = Number(this.queryStr.page) || 1;
     const skip = resPerPage * (currentPage - 1);
